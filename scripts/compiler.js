71b0540623a02b9fd3a5c7e715647e42044f3fdc
Calin Pirtea <pcalin@yahoo.com>
2023-06-22 16:55:09 +0800

    Improved render using Compiled graph
    Co-authored-by: ZXMushroom63 <ZXMushroom63@users.noreply.github.com>

--- scripts/compiler.js
+++ scripts/compiler.js
@@ -1,6 +1,8 @@
 function compileGraph(node) {
   var libraryEntry = window["library"][node.getAttribute("data-type")];
   var rows = node.querySelectorAll("tr");
+  console.log(libraryEntry);
+  console.log(node);
   var func = libraryEntry.func;
   var argv = [];
   for (let i = 0; i < rows.length; i++) {
@@ -23,15 +25,15 @@ function compileGraph(node) {
     argv: argv,
     calculate: function () {
       var values = [];
-      for (let arg = 0; arg < argv.length; arg++) {
-        const argument = argv[arg];
+      for (let arg = 0; arg < this.argv.length; arg++) {
+        const argument = this.argv[arg];
         if (typeof argument === "object") {
-          return argument.calculate();
+          values.push(argument.calculate());
         } else {
           values.push(argument || 0);
         }
       }
-      return func(...values) | 0;
+      return this.func(...values) || 0;
     },
   };
 }
