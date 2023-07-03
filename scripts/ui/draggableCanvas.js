97da86602516c69f7e501c33b7e90b37412716f9
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2023-07-03 14:26:29 +0800

    Scrolling patch to make things work way smoother

--- scripts/ui/draggableCanvas.js
+++ scripts/ui/draggableCanvas.js
@@ -5,10 +5,10 @@ var scrollingUI = {
   oldX: 0,
   oldY: 0,
 };
-document.querySelector("body").addEventListener("contextmenu", (e) => {
+document.querySelector("html").addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
-document.querySelector("body").addEventListener("mousedown", (e) => {
+document.querySelector("html").addEventListener("mousedown", (e) => {
   if (e.button === 2) {
     scrollingUI.scrolling = true;
     scrollingUI.oldX = e.x;
@@ -16,13 +16,11 @@ document.querySelector("body").addEventListener("mousedown", (e) => {
   }
 });
 
-document.querySelector("body").addEventListener("mouseup", (e) => {
-  if (e.button === 2) {
-    scrollingUI.scrolling = false;
-  }
+document.querySelector("html").addEventListener("mouseup", (e) => {
+  scrollingUI.scrolling = false;
 });
 
-document.querySelector("body").addEventListener("mousemove", (e) => {
+document.querySelector("html").addEventListener("mousemove", (e) => {
   if (scrollingUI.scrolling) {
     scrollingUI.x += e.x - scrollingUI.oldX;
     scrollingUI.y += e.y - scrollingUI.oldY;
@@ -30,6 +28,9 @@ document.querySelector("body").addEventListener("mousemove", (e) => {
     scrollingUI.oldY = e.y;
     updateScroll();
   }
+});
+window.addEventListener("blur", function (e) {
+  scrollingUI.scrolling = false;
 });
 function updateScroll() {
   document.querySelector(
@@ -45,6 +46,24 @@ function updateScroll() {
   });
 }
 document.querySelector("#toolbar").addEventListener("mousedown", function (e) {
+  if (e.button === 2) {
+    e.stopPropagation;
+    e.stopImmediatePropagation();
+  }
+});
+document.querySelector("#zoomout").addEventListener("mousedown", function (e) {
+  if (e.button === 2) {
+    e.stopPropagation;
+    e.stopImmediatePropagation();
+  }
+});
+document.querySelector("#zoomin").addEventListener("mousedown", function (e) {
+  if (e.button === 2) {
+    e.stopPropagation;
+    e.stopImmediatePropagation();
+  }
+});
+document.querySelector("#trashbin").addEventListener("mousedown", function (e) {
   if (e.button === 2) {
     e.stopPropagation;
     e.stopImmediatePropagation();
